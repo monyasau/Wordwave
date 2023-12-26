f7974da783001a51cdea2b52795441f1156cca0a
Olajide Olanrewaju <Olajide.m.olanrewaju@gmail.com>
2023-12-26 14:38:25 +0100

    app is functional 🎉

--- src/components/Dictionary.jsx
+++ src/components/Dictionary.jsx
@@ -1,41 +1,79 @@
-import React, { useState, useEffect  } from 'react';
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-let Dictionary = ()=> {
-const [userInput, setUserInput] = useState("hello");
-const [dictData, setDictData] = useState(null)
+let Dictionary = () => {
+  const [userInput, setUserInput] = useState();
+  const [dictData, setDictData] = useState(null);
 
-const updateUserInput = (event)=>{
-    setUserInput(event.target.value)
-}
+  const updateUserInput = (event) => {
+    if (event.keyCode === 13) {
+      setUserInput(event.target.value);
+    }
+  };
 
+  useEffect(() => {
+    axios
+      .get(`https://api.dictionaryapi.dev/api/v2/entries/en/${userInput}`)
+      .then(function (response) {
+        setDictData(response.data);
+        console.log(dictData);
+      });
+    //   .catch(function (error) {
+    //     setDictData(error.data.title)
+    //   })
+    {
+      setDictData(null);
+    }
+  }, [userInput]);
 
-useEffect(()=>{
- axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${userInput}`)
-  .then(function (response) {
-    
-    setDictData(response.data)
-  })
-  .catch(function (error) {
-    console.log(error);
-  })
+  return (
+    <>
+      <div className="">
+        <div className="max-w-screen-sm mx-auto md:py-36 py-12">
+          <div className="w-full border rounded h-16 text-3xl">
+            <input
+              className="bg-[#f4f4f4] w-[96%] h-full px-4"
+              type="text"
+              autoFocus
+              onKeyUp={updateUserInput}
+            />
+            <button className="w-[4%]" onClick={updateUserInput}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="#b9b9b9"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+                />
+              </svg>
+            </button>
+          </div>
+          <h1 className="text-darkGray mb-3 text-3xl font-bold">
+            Word:{userInput}{" "}
+          </h1>
 
-}
-,[userInput])
-
-    return (
-        <div>
-<p>count: {userInput} </p>
-{/* <button onClick={updateDict()}></button> */}
-<input className='border-b' type="text" autoFocus onKeyUp={updateUserInput}/>
-{dictData?<div>{dictData[0].word}</div>
-:<div>not found</div>
-}
-
+          {dictData ? (
+            <div className="">
+              Definitions :{" "}
+              {dictData[0].meanings.map((meaning, i) => (
+                <div key={i}>
+                  <li> {dictData[0].meanings[i].definitions[0].definition}</li>
+                </div>
+              ))}
+            </div>
+          ) : (
+            <div>Sorry, definition not found</div>
+          )}
         </div>
-    );
-}
+      </div>
+    </>
+  );
+};
 
 export default Dictionary;
-
-
