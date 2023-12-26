1464f14aa1091e93a2bfb9eedc359df72a226fb1
Olajide Olanrewaju <Olajide.m.olanrewaju@gmail.com>
2023-12-26 12:36:49 +0100

    update: added input support, dictionary component

--- src/components/Dictionary.jsx
+++ src/components/Dictionary.jsx
@@ -1,9 +1,25 @@
-function Dictionary() {
+import React, { useState, useEffect  } from 'react';
+import axios from 'axios';
+
+let Dictionary = ()=> {
+const [userInput, setUserInput] = useState("hello");
+const [dictData, setDictData] = useState(null)
+
+const updateUserInput = (event)=>{
+    setUserInput(event.target.value)
+}
+
     return (
         <div>
-            Enter
+<p>count: {userInput} </p>
+{/* <button onClick={updateDict()}></button> */}
+<input className='border-b' type="text" autoFocus onKeyUp={updateUserInput}/>
+
+
         </div>
     );
 }
 
 export default Dictionary;
+
+
