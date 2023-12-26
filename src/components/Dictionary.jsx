ed47694b025ba1b1b320975a5cf71b6bcfb81e19
Olajide Olanrewaju <Olajide.m.olanrewaju@gmail.com>
2023-12-26 12:37:20 +0100

    added fetch capabilities using axios

--- src/components/Dictionary.jsx
+++ src/components/Dictionary.jsx
@@ -9,12 +9,28 @@ const updateUserInput = (event)=>{
     setUserInput(event.target.value)
 }
 
+
+useEffect(()=>{
+ axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${userInput}`)
+  .then(function (response) {
+    
+    setDictData(response.data)
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+
+}
+,[userInput])
+
     return (
         <div>
 <p>count: {userInput} </p>
 {/* <button onClick={updateDict()}></button> */}
 <input className='border-b' type="text" autoFocus onKeyUp={updateUserInput}/>
-
+{dictData?<div>{dictData[0].word}</div>
+:<div>not found</div>
+}
 
         </div>
     );
