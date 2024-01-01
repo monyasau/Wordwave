c34000b40b8cf4a99026cece34221f39b3ea8448
Olajide Olanrewaju <Olajide.m.olanrewaju@gmail.com>
2024-01-01 22:07:40 +0100

    Major ui redesign

--- src/components/Dictionary.jsx
+++ src/components/Dictionary.jsx
@@ -2,59 +2,83 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 let Dictionary = () => {
-  const [userInput, setUserInput] = useState("hello");
-  const [inputValue, setInputValue] = useState("");
-  const [dictData, setDictData] = useState(null);
+  const [query, setQuery] = useState("");
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [dictData, setDictData] = useState([]);
 
   const updateUserInput = (event) => {
+    setError(false);
+
     if (event.keyCode === 13 || event.type === "click") {
-      setUserInput(event.target.value);
+      fetchDictionaryData();
     } else {
-      //for button
-      setInputValue(event.target.value);
+      setQuery(event.target.value);
     }
   };
-  const buttonUpdateDict = () => {
-    setUserInput(inputValue);
-  };
 
-  const fetchDictionaryData = () => {
-    
-    axios
-      .get(`https://api.dictionaryapi.dev/api/v2/entries/en/${userInput}`)
+  const fetchDictionaryData = async () => {
+    setDictData([]);
+    setLoading(true);
+
+    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${query}`;
+    await axios
+      .get(url)
       .then(function (response) {
         const newData = response.data;
         setDictData(newData);
         console.log(newData);
       })
       .catch(function (error) {
-        setDictData(null);
+        setError(true);
       });
+
+    setLoading(false);
   };
-  useEffect(() => {
-    fetchDictionaryData();
-  }, [userInput]);
+
+  console.log(window.navigator.onLine);
+  // useEffect(() => {
+  //   fetchDictionaryData();
+  // }, [query]);
 
   return (
-    <>
+    <div className="flex-grow p-4">
       <div className="">
         <div className="max-w-screen-sm mx-auto py-12">
-          <div className="w-full border rounded h-16 md:text-3xl text-2xl">
+          <h1 className="text-center text-xl font-medium py-4">WordWave</h1>
+
+          <div className="w-full border rounded bg-white text-lg flex items-center overflow-hidden p-4">
             <input
               placeholder="Enter a word here to search"
-              className="bg-[#f4f4f4] md:w-[96%] w-[85%]  h-full px-4"
+              className="flex-grow h-full focus:outline-none"
               type="text"
               autoFocus
               onKeyUp={updateUserInput}
             />
-            <button className="md:w-[4%] w-[15%] " onClick={buttonUpdateDict}>
+            {loading ? (
               <svg
-                xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
                 strokeWidth={1.5}
-                stroke="#b9b9b9"
-                className="w-6 mx-auto h-6"
+                stroke="currentColor"
+                className="w-6 h-6 text-gray-500 animate-spin"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+                />
+              </svg>
+            ) : (
+              <svg
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6 text-gray-500"
+                onClick={fetchDictionaryData}
+                xmlns="http://www.w3.org/2000/svg"
               >
                 <path
                   strokeLinecap="round"
@@ -62,67 +86,82 @@ let Dictionary = () => {
                   d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
                 />
               </svg>
-            </button>
+            )}
           </div>
-          <h1 className="text-[#455] mb-3 text-3xl font-bold text-center">
-            {userInput.toLocaleUpperCase()}
-          </h1>
 
-          {dictData ? (
+          {dictData.length ? (
             <div className="">
-              <p className="pl-6">
-                Definitions, Synonyms, Phonetics & Usage Examples of <span className="font-bold"> {userInput}</span> :
+              <p className="text-[#455] p-4 text-lg text-center">
+                Search Results for: "{query}"
               </p>
 
-              {dictData[0].meanings[0].synonyms.length? (
-                <div className="border my-4 p-6">
+              <p className="pl-6">
+                Definitions, Synonyms, Phonetics & Usage Examples of{" "}
+                <span className="font-bold"> {query}</span> :
+              </p>
+
+              {dictData[0].meanings[0].synonyms.length ? (
+                <div className="border bg-white my-4 p-6">
                   {<span className="font-bold"> Synonyms:</span>}
-                  {
-                    dictData[0].meanings[0].synonyms.map((synonym,synonymIteration)=>(
-                      <span key={synonymIteration} className="text-[#444] text-base"> {synonym},</span>
-                    ))
-                  }
+                  {dictData[0].meanings[0].synonyms.map(
+                    (synonym, synonymIteration) => (
+                      <span
+                        key={synonymIteration}
+                        className="text-[#444] text-base"
+                      >
+                        {" "}
+                        {synonym},
+                      </span>
+                    )
+                  )}
                 </div>
-              ):
-              <div className="border my-4 p-6">
-                <span className="font-bold">Synonyms: </span> <span className="text-[#444]"> No synonyms found</span>
-              </div>
-              }
+              ) : (
+                <div className="border bg-white my-4 p-6">
+                  <span className="font-bold">Synonyms: </span>{" "}
+                  <span className="text-[#444]"> No synonyms found</span>
+                </div>
+              )}
 
               {dictData[0].meanings.map((meaning, i) => (
-                <div key={i} className="border my-2 p-6">
+                <div key={i} className="border  bg-white my-2 p-6">
                   <h4>
                     ({dictData[0].meanings[i].partOfSpeech.toUpperCase()})
                   </h4>
 
                   {dictData[0].meanings[i].definitions.map((definition, j) => (
                     <>
-                    <div key={j} className="border my-4 grid grid-cols-2">
-                      <div className="border px-1  flex flex-wrap items-center justify-center">
-                        <p>
-                          <span className="font-bold">Definition: </span>
-                          {definition.definition}
-                        </p>
-                        {definition.example && (
+                      <div key={j} className="my-4 grid grid-cols-2">
+                        <div className="border p-4  flex flex-col justify-center">
                           <p>
-                            <span  className="font-bold">Usage Example: </span>
-                            {definition.example}
+                            <span className="font-bold">Definition: </span>
+                            {definition.definition}
                           </p>
-                        )}
+                          {definition.example && (
+                            <p>
+                              <span className="font-bold">Usage Example: </span>
+                              {definition.example}
+                            </p>
+                          )}
+                        </div>
+                        <div className="border p-4 items-center justify-center">
+                          <p className="text-center pb-2">
+                            <span className="font-bold">Phonetic: </span>{" "}
+                            {dictData[0].phonetic}
+                          </p>
+                          {dictData &&
+                            dictData[0]?.phonetics &&
+                            dictData[0]?.phonetics[0] && (
+                              <audio controls className="w-2/3 mx-auto">
+                                <source
+                                  src={dictData[0].phonetics[0].audio}
+                                  type="audio/mp3"
+                                />
+                                Your browser does not support or has disabled
+                                playing audio
+                              </audio>
+                            )}
+                        </div>
                       </div>
-                      <div className="border px-1 flex flex-wrap items-center justify-center">
-                        <p>
-                          <span  className="font-bold">Phonetic: </span> {dictData[0].phonetic}
-                        </p>
-                       {dictData && dictData[0]?.phonetics && dictData[0]?.phonetics[0] && (
-  <audio controls className="w-[80%]">
-    <source src={dictData[0].phonetics[0].audio} type="audio/mp3" />
-    Your browser does not support or has disabled playing audio
-  </audio>
-)}
-
-                      </div>
-                    </div>
                     </>
                   ))}
                 </div>
@@ -130,18 +169,23 @@ let Dictionary = () => {
             </div>
           ) : (
             <>
-              <div className="text-center border text-4xl">
-                Sorry, definition not found 🥺
-              </div>
-              <div className="md:text-1xl md:my-8 w-[90%] mx-auto text-center text-base">
-                Try checking your internet connection, if that doesn't work, try
-                again later
-              </div>
+              {query && error ? (
+                <div className="py-4">
+                  <div className="text-center text-lg">
+                    Sorry, definition not found 🥺
+                  </div>
+                </div>
+              ) : (
+                <div className="text-sm text-center p-4">
+                  Dive into the knowledge ocean! What are you searching for
+                  today?
+                </div>
+              )}
             </>
           )}
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
