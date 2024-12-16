1720888dafac1d38f4186cd001aac623da88c70c
Olajide Olanrewaju <olajide.m.olanrewaju@gmail.com>
2024-12-16 23:49:15 +0100

    update:  incorrect protocol and name format

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,10 @@ const Footer = () => {
   return (
     <div className="text-center text-sm p-8 bg-white/70 border-t">
     Made with &hearts; by <a
-        href="http://monyasau.netlify.app"
+        href="https://monyasau.netlify.app"
         target="_blank"
         className="underline"
-      >Olajide Olanrewaju</a>
+      >Olanrewaju Olajide</a>
     </div>
   );
 };
